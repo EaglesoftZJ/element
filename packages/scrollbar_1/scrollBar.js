911a5ca666743bf504592e9e2315b92e85ed69f3
n1ghtw1sh <116516842@qq.com>
2018-04-12 09:31:43 +0800

    eslint

--- packages/scrollbar_1/scrollBar.js
+++ packages/scrollbar_1/scrollBar.js
@@ -3,42 +3,42 @@
  */
 import scrollBar from 'perfect-scrollbar';
 export default {
-    instance: null,
-    refArr: {},
-    use(refs, instance) {
-        this.instance = instance;
-        this.refArr[instance._uid] = refs;
-        for (let key in refs) {
-            if (!/^scroll/.test(key)) {
-                continue;
-            }
-            this.init(refs[key]);
-        }
-    },
-    destroy(instance) {
-        delete this.refArr[instance._uid];
-    },
-    init(ref) {
-        scrollBar.initialize(ref);
-    },
-    scrollTo(ref, scrollTop) {
-        ref.scrollTop = scrollTop;
-        scrollBar.update(ref);
-    },
-    update(ref) {
-        scrollBar.update(ref);
+  instance: null,
+  refArr: {},
+  use(refs, instance) {
+    this.instance = instance;
+    this.refArr[instance._uid] = refs;
+    for (let key in refs) {
+      if (!/^scroll/.test(key)) {
+        continue;
+      }
+      this.init(refs[key]);
     }
-    // resize() {
-    //     let self = this;
-    //     $(window).on('resize', function() {
-    //         for (let UID in self.refArr) {
-    //             for (let key in self.refArr[UID]) {
-    //                 if (!/^scroll/.test(key)) {
-    //                     continue;
-    //                 }
-    //                 self.update(self.refArr[UID][key]);
-    //             }
-    //         }
-    //     });
-    // }
+  },
+  destroy(instance) {
+    delete this.refArr[instance._uid];
+  },
+  init(ref) {
+    scrollBar.initialize(ref);
+  },
+  scrollTo(ref, scrollTop) {
+    ref.scrollTop = scrollTop;
+    scrollBar.update(ref);
+  },
+  update(ref) {
+    scrollBar.update(ref);
+  }
+  // resize() {
+  //     let self = this;
+  //     $(window).on('resize', function() {
+  //         for (let UID in self.refArr) {
+  //             for (let key in self.refArr[UID]) {
+  //                 if (!/^scroll/.test(key)) {
+  //                     continue;
+  //                 }
+  //                 self.update(self.refArr[UID][key]);
+  //             }
+  //         }
+  //     });
+  // }
 };
