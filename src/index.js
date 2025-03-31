97033089df6c284f7012c2b06f3060d805d6828e
beizhuxin <438830119@qq.com>
2025-03-31 14:55:26 +0800

    2.5.34-beta3

--- src/index.js
+++ src/index.js
@@ -197,7 +197,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 module.exports = {
-  version: '2.5.34-beta2',
+  version: '2.5.34-beta3',
   locale: locale.use,
   i18n: locale.i18n,
   install,
