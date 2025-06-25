c8b9c8f9bee943a0dc530b53ed0f52db27c45afb
beizhuxin <438830119@qq.com>
2025-06-25 16:43:59 +0800

    eg-element-ui@2.5.39-beta1

--- src/index.js
+++ src/index.js
@@ -198,7 +198,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 module.exports = {
-  version: '2.5.38-beta1',
+  version: '2.5.39-beta1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
