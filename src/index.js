eb4a7fb7690cc86f268c83a55e05b2f7c2527949
beizhuxin <438830119@qq.com>
2025-07-23 09:13:30 +0800

    2.5.40-beta2

--- src/index.js
+++ src/index.js
@@ -198,7 +198,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 module.exports = {
-  version: '2.5.40-beta1',
+  version: '2.5.40-beta2',
   locale: locale.use,
   i18n: locale.i18n,
   install,
