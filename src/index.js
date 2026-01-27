f055acfc0e98b472cdb73db4c2168d0826101041
beizhuxin <438830119@qq.com>
2026-01-27 14:44:11 +0800

    2.5.46-beta1

--- src/index.js
+++ src/index.js
@@ -200,7 +200,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 module.exports = {
-  version: '2.5.45-beta2',
+  version: '2.5.46-beta1',
   locale: locale.use,
   i18n: locale.i18n,
   install,
