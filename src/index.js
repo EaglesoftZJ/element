62523d4376db97803393f5c928e1f19576af49f4
beizhuxin <438830119@qq.com>
2026-04-21 16:03:28 +0800

    2.5.48-beta6

--- src/index.js
+++ src/index.js
@@ -200,7 +200,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 module.exports = {
-  version: '2.5.47-beta2',
+  version: '2.5.48-beta6',
   locale: locale.use,
   i18n: locale.i18n,
   install,
